0f8b5a12da7c60845fcd1df33b885c0b07f86d10
Matheus <matheus.b.correia7@gmail.com>
2023-11-20 23:01:57 -0300

    update

--- app.js
+++ app.js
@@ -1,43 +1,86 @@
-// let mensagemDeErro = "Erro! Preencha todos os campos";
-// alert(mensagemDeErro);
-// let nome = prompt("Qual é o seu nome?");
-// let idade = prompt("Qual é sua idade?");
-// if (idade >= 18) {
-//   alert("Pode tirar a habilitacão!");
-// } else {
-//   let idadeFaltando;
-//   if (18 - idade == 1) {
-//     idadeFaltando = "ainda falta 1 ano para poder tirar a habilitacão!";
+// alert("Welcome to the secret number game!");
+
+// let numeroSecreto = parseInt(Math.random() * 100 + 1);
+
+// console.log(numeroSecreto);
+
+// let chute;
+// let tentativas = 1;
+
+// while (chute != numeroSecreto) {
+//   chute = prompt("Choose a number between 1 and 100");
+//   if (chute == numeroSecreto) {
+//     break;
 //   } else {
-//     idadeFaltando = `ainda faltam ${
-//       18 - idade
-//     } anos para poder tirar a habilitacão!`;
+//     if (numeroSecreto > chute) {
+//       alert(`O número secreto é maior que ${chute}!`);
+//     } else {
+//       alert(`O número secreto é menor que ${chute}!`);
+//     }
+//     tentativas++;
 //   }
-//   alert(idadeFaltando);
 // }
 
-alert("Welcome to the secret number game!");
+// let palavraTentativa = tentativas > 1 ? "tentativas" : "tentativa";
 
-let numeroSecreto = 12;
+// alert(
+//   `🎉Você acertou!!! O número secreto é ${numeroSecreto}, você descobriu o número sercreto em ${tentativas} ${palavraTentativa}🎉`
+// );
 
-console.log(numeroSecreto);
+// console.log("Boas vindas!!!");
+// let nome = "Matheus";
+// alert(`Olá ${nome}`);
+// linguagemPreferida = prompt(
+//   "Qual a linguagem de programação que você mais gosta?"
+// );
+// alert(`Ah, ${linguagemPreferida}! Que legal!!!`);
+// {
+//   alert(`Iniciando adição...`);
+//   let valor1 = prompt("insira um valor para somar");
+//   let valor2 = prompt("insira outro valor para somar");
+//   let resultado = +valor1 + +valor2;
+//   alert(`A soma de ${valor1} e ${valor2} é ${resultado}`);
+// }
 
-let chute;
-let tentativas = 1;
+// {
+//   alert(`Iniciando subtração...`);
+//   let valor1 = prompt("insira um valor para subtrair");
+//   let valor2 = prompt("insira outro valor para subtrair");
+//   let resultado = +valor1 - +valor2;
+//   alert(`A diferença entre ${valor1} e ${valor2} é ${resultado}`);
+// }
+// alert("Verificar maioridade");
+// let idade = prompt("Insira sua idade aqui");
+// if (idade >= 18) {
+//   alert("você é de maior");
+// } else {
+//   alert("você é de menor");
+// }
+// let numero = prompt("insira um numero");
+// if (numero > 0) {
+//   alert("número positivo");
+// } else {
+//   if (numero == 0) {
+//     alert("numero neutro");
+//   } else {
+//     alert("numero negativo");
+//   }
+// }
 
-while (chute != numeroSecreto) {
-  chute = prompt("Choose a number between 1 and 100");
+// let i = 0;
 
-  if (chute == numeroSecreto) {
-    alert(
-      `🎉You won!!! The secret number really is ${numeroSecreto}, you tryed ${tentativas}x!🎉`
-    );
-  } else {
-    if (numeroSecreto > chute) {
-      alert(`O número secreto é maior que ${chute}!`);
-    } else {
-      alert(`O número secreto é menor que ${chute}!`);
-    }
-    tentativas++;
-  }
-}
+// while (i < 10) {
+//   i++;
+//   console.log(i);
+// }
+
+// let nota = parseInt(Math.random() * 10);
+// let statusNota = nota >= 7 ? "aprovado" : "recuperação";
+// console.log(`Sua nota foi ${nota} e seu status é: ${statusNota}`);
+
+let randomNumero1 = Math.random();
+alert(randomNumero1);
+let randomNumero2 = parseInt(Math.random() * 10 + 1);
+alert(randomNumero2);
+let randomNumero3 = parseInt(Math.random() * 1000 + 1);
+alert(randomNumero3);
